dfcf1b40715e2301f082721a8303f4c0475546ab
Maksim Rakitin <mrakitin@bnl.gov>
2021-02-08 13:10:06 -0500

    Fix OSX build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,9 @@
+set -e -x
+
+if [ "$(uname)" = "Darwin" ]; then
+    CFLAGS+=" -Wno-error=deprecated-declarations "
+fi
+
 ./configure --enable-python --disable-perl --disable-java --disable-ruby \
 	    --disable-fortran2003 --disable-lua --prefix=$PREFIX
 make
